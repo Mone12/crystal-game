dd196694fa7d225f4fc70cca0a22bd491536e62a
Mone Duncans-Francis <mone.eunique12@gmail.com>
2019-11-12 20:26:18 -0800

    adding value to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,22 +4,38 @@ var losses = 0;
 var counter =0;
 
 // make target number random between 19-120
-var targetNumber = Math.floor(Math.random() * (102) ) + 19;
+var targetNumber = Math.floor(Math.random() * 102)  + 19;
 
-// make hidden number value random between 1-12
-var numberOptions = Math.floor(Math.random() * (12) ) + 1;
+// make hidden number value random between 1-12 for each crystal
+var blueCrystal = Math.floor(Math.random() * 12) + 1;
+var yellowCrystal = Math.floor(Math.random() * 12) + 1;
+var redCrystal = Math.floor(Math.random() * 12)  + 1;
+var greenCrystal = Math.floor(Math.random() * 12) + 1;
 
-// append target number to h1 element
+// Display target number and user's current score
 $("#number-to-guess").text(targetNumber);
+$(".counter-number").text("Your score:" + counter);
 
-for (var i = 0; i < numberOptions.length; index++) {
-    $(".crystal-image").attr("data-crystalvalue", numberOptions[i]);
-    
-}
 
-// make the images clickable
-$(".crystal-image").on("click", function() {
-alert("Hello");
+
+
+// make red crystal clickable
+$(".red").on("click", function() {
+counter = counter + redCrystal;
+$(".counter-number").text("Your score:" + counter);
 })
-
-
+// make blue crystal clickable
+$(".blue").on("click", function() {
+    counter = counter + blueCrystal;
+    $(".counter-number").text("Your score:" + counter);
+    })
+// make yellow crystal clickable
+$(".yellow").on("click", function() {
+    counter = counter + yellowCrystal;
+    $(".counter-number").text("Your score:" + counter);
+    })
+// make green crystal clickable
+$(".green").on("click", function() {
+    counter = counter + greenCrystal;
+    $(".counter-number").text("Your score:" + counter);
+    })
